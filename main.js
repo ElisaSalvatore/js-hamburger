08fbd8c745fae2177b7bb1d154d0fd4fedf70e27
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-10 10:04:51 +0100

    hamburger menu js fixed

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const elementHamMenu = document.querySelector(".fa-bars");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+//creo le due costanti:
+//una per il bottone dell'hamburger menu e l'altro per il menu mobile
+const hamburgerButton = document.querySelector(".header-right > a");
+const closeMenu = document.querySelector(".hamburger-menu .close");
 
-elementHamMenu.addEventListener ('click', function() {
-    hamburgerMenu.classList.add('active');
-})
+const hamburgerMenuMobile = document.querySelector(".hamburger-menu");
 
-const closeMenu = document.querySelector(".fa-times");
+//attivare l'hamburger menu: al click sul bottone si apre il menu mobile
+hamburgerButton.addEventListener ('click', function() {
+    hamburgerMenuMobile.classList.add('active');
+});
 
+//click sulla x per chiudere il menu mobile
 closeMenu.addEventListener ('click', function() {
-    hamburgerMenu.classList.remove('active');
-})
+    hamburgerMenuMobile.classList.remove('active');
+});
